0e65dd8c2d6835454b7d3f37eb34582af87bdb70
BittnerPierre <pierre@MBP-de-Pierre.home>
2015-08-14 11:08:17 +0200

    second commit

---
+++ src/main/java/fr/finaxys/App.java
@@ -0,0 +1,13 @@
+package fr.finaxys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
