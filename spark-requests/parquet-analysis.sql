46be329a61819bdc122d328c25d473ac34110c53
mehdibenissa <mehdi_benissa@yahoo.fr>
2015-12-09 15:17:01 +0100

    Update parquet-analysis.sql

--- spark-requests/parquet-analysis.sql
+++ spark-requests/parquet-analysis.sql
@@ -1,1 +1,1 @@
-SELECT Order.ObName , Order.Trace , order.Quantity FROM records where type='Order'
+SELECT Order.ObName as OrderBook , SUM(order.Quantity) as TotalQuantity FROM records where type='Order' GROUP BY Order.ObName ORDER BY TotalQuantity DESC
