059e196f8a85cc204bf20185f302db4f381d6b89
mehdi <mehdi_benissa@yahoo.fr>
2015-12-09 15:50:59 +0100

    edit spark requests

--- spark-requests/hbase-analysis.sql
+++ spark-requests/hbase-analysis.sql
@@ -1,1 +1,1 @@
-SELECT * FROM records
+SELECT obName as OrderBook , SUM(quantity) as TotalQuantity FROM records where trace='Order' GROUP BY obName ORDER BY TotalQuantity DESC
--- spark-requests/hbase-streaming-analysis.sql
+++ spark-requests/hbase-streaming-analysis.sql
@@ -1,1 +1,1 @@
-SELECT * from records
+SELECT obName as OrderBook , SUM(quantity) as TotalQuantity FROM records where trace='Order' GROUP BY obName ORDER BY TotalQuantity DESC
