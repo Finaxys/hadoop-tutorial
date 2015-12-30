e10abf26ae37cb0ac4be301211a9a8a33609dd93
mehdi <mehdi_benissa@yahoo.fr>
2015-12-30 17:43:38 +0100

    GenericRecord -->VRcord

--- src/main/java/fr/finaxys/tutorials/utils/parquet/AvroParquetConverterMapper.java
+++ src/main/java/fr/finaxys/tutorials/utils/parquet/AvroParquetConverterMapper.java
@@ -1,7 +1,7 @@
 package fr.finaxys.tutorials.utils.parquet;
 
 
-import org.apache.avro.generic.GenericRecord;
+import fr.finaxys.tutorials.utils.avro.models.VRecord;
 import org.apache.avro.mapred.AvroKey;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -9,10 +9,10 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AvroParquetConverterMapper extends
-        Mapper<AvroKey<GenericRecord>, NullWritable, Void, GenericRecord> {
+        Mapper<AvroKey<VRecord>, NullWritable, Void, VRecord> {
 
     @Override
-    protected void map(AvroKey<GenericRecord> key, NullWritable value,
+    protected void map(AvroKey<VRecord> key, NullWritable value,
                        Context context) throws IOException, InterruptedException {
         context.write(null, key.datum());
     }
